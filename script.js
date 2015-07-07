2de41807da0f4cd142b09e90091f0b0abb04fe1d
Rob <robbcardenas@gmail.com>
2015-07-06 18:30:32 -0700

    Pulled data from Spotify and rendered it to the index.html

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 $(function() {
 
-  // your code here
+	// underscore template
+    var trackTemplate = _.template($('#trackArtist-template').html());
+
+	// form to search spotify API
+	var $spotifySearch = $('#spotify-search');
+
+	// form input for track (song)
+	var $track = $('#track');
+
+	// element to hold results from spotify API
+	var $results = $('#results');
+
+	// Submit form to search spotify API
+	$spotifySearch.on('submit', function(event) {
+		event.preventDefault();
+
+  	$.get(
+  		'https://api.spotify.com/v1/search?type=track&q=' + $track.val(),
+  		function(data) {
+  			console.log(data);
+  			var tracksLength = data.tracks.items.length;
+  			for (var i = 0; i < tracksLength; i += 1) {
+  				if ( tracksLength > 0 ) {
+  					trackName = data.tracks.items[i].name;
+  					artistName = data.tracks.items[i].artists[0].name;
+  					albumName = data.tracks.items[i].album.name;
+  					$results.append(trackTemplate(trackName,artistName,albumName));
+  				}
+  			}
+  		}
+  	);
+
+  });
 
 });
+
+
+
+
+
+
